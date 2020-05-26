3af059f84036555f2bf87478367744efee63f2bc
Gurminder <gurminder@leena.ai>
2020-05-26 13:53:09 +0530

    added fix for font

--- pages/scheduleDemo/index.js
+++ pages/scheduleDemo/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import ScheduleDemo from '../../containers/ScheduleDemo';
 
-export default function Home() {
+export default function Schedule() {
   return (
     <div className="container">
       <Head>
