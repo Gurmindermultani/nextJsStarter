88f6e2445383f024adaba06ae1cacbc0f533cd22
Gurminder <gurminder@leena.ai>
2020-07-17 15:24:52 +0530

    added 301

--- pages/_error.js
+++ pages/_error.js
@@ -86,7 +86,7 @@ function CustomError({ statusCode }) {
 
 function getInitialProps({ req, res, err }) {
   if (req.url && redirections[req.url.replace(/\/$/, "")]) {
-    res.writeHead(302, { Location: redirections[req.url.replace(/\/$/, "")] }).end();
+    res.writeHead(301, { Location: redirections[req.url.replace(/\/$/, "")] }).end();
   }
   let statusCode;
   // If the res variable is defined it means nextjs
