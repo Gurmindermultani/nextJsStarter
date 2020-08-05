45bbd84c31f9d41609f4a967faa06afffc0c73e1
Gurminder <gurminder@leena.ai>
2020-08-05 14:34:33 +0530

    added blog full width

--- containers/Blog/styles.js
+++ containers/Blog/styles.js
@@ -5,12 +5,11 @@ export const AboutUsStyles = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-around;
-    max-width: 1200px;
     margin: auto;
     &.section0 {
       padding: 80px 0px 0 0px;
       iframe {
-        
+
       }
     }
     &.noMargin {
